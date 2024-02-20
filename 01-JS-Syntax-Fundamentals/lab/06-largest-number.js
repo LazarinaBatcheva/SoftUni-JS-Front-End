af8d2820f169a015167d29ca8a7e116c1b561831
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-02-20 13:17:09 +0100

    Add files via upload

---
+++ 01-JS-Syntax-Fundamentals/lab/06-largest-number.js
@@ -0,0 +1,10 @@
+function largestNumber(num1, num2, num3) {
+    let largest = Math.max(num1, num2, num3);
+    console.log(`The largest number is ${largest}.`);
+}
+
+
+// test code
+
+largestNumber(5, -3, 16);
+largestNumber(-3, -5, -22.5);
