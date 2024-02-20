973ab2d716fdd1ad8706a1c1444da2d95602f8a0
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-02-20 12:13:13 +0100

    Add files via upload

---
+++ 01-JS-Syntax-Fundamentals/lab/03-excellent-grade.js
@@ -0,0 +1,13 @@
+function solve(grade) {
+    if(grade >= 5.50) {
+        console.log('Excellent');
+    } else {
+        console.log('Not excellent');
+    }
+}
+
+
+// test code
+
+solve(5.50);
+solve(4.35);
