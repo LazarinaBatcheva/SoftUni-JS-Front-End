8735fbe04787700949f5fce7d4e91b67bc6e1963
Lazarina-Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-02-20 12:38:39 +0100

    added new task`s solve

--- 01-JS-Syntax-Fundamentals/lab/04-month -printer.js
+++ 01-JS-Syntax-Fundamentals/lab/04-month -printer.js
@@ -1,45 +1,61 @@
-function solve(month) {
-    switch(month) {
-        case 1:
-            console.log('January');
-            break
-        case 2:
-            console.log('February');
-            break
-        case 3:
-            console.log('March');
-            break
-        case 4:
-            console.log('April');
-            break
-        case 5:
-            console.log('May');
-            break
-        case 6:
-            console.log('June');
-            break
-        case 7:
-            console.log('July');
-            break
-        case 8:
-            console.log('August');
-            break
-        case 9:
-            console.log('September');
-            break
-        case 10:
-            console.log('October');
-            break
-        case 11:
-            console.log('November');
-            break
-        case 12:
-            console.log('December');
-            break
-        default:
-            console.log('Error!');
+function solve(number) {
+    const months = [
+        'January', 'February', 'March',
+        'April', 'May', 'June',
+        'July', 'August', 'September',
+        'October', 'November', 'December'
+    ];
+
+    if (number < 1 || number > 12) {
+        console.log('Error!');
+    } else {
+        console.log(months[number - 1]);
     }
 }
+
+
+// function solve(month) {
+//     switch(month) {
+//         case 1:
+//             console.log('January');
+//             break
+//         case 2:
+//             console.log('February');
+//             break
+//         case 3:
+//             console.log('March');
+//             break
+//         case 4:
+//             console.log('April');
+//             break
+//         case 5:
+//             console.log('May');
+//             break
+//         case 6:
+//             console.log('June');
+//             break
+//         case 7:
+//             console.log('July');
+//             break
+//         case 8:
+//             console.log('August');
+//             break
+//         case 9:
+//             console.log('September');
+//             break
+//         case 10:
+//             console.log('October');
+//             break
+//         case 11:
+//             console.log('November');
+//             break
+//         case 12:
+//             console.log('December');
+//             break
+//         default:
+//             console.log('Error!');
+//     }
+// }
 
 
 // test code
