48d9216f25c0e4e99ca54ab337e385fd6f84da20
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-02-21 16:25:45 +0100

    Add files via upload

---
+++ 01-JS-Syntax-Fundamentals/lab/10-numbers-from-m-to-n.js
@@ -0,0 +1,11 @@
+function printNumbersReversed(m, n) {
+    for (let i = m; i >= n; i--) {
+        console.log(i);
+    }
+}
+
+
+// test code
+
+printNumbersReversed(6, 2);
+printNumbersReversed(4, 1);
