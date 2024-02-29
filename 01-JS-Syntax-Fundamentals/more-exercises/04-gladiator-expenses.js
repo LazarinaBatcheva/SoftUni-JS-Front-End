8de41284ad510c4d5a97c27f2197f542ec96b880
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-02-29 13:24:19 +0100

    Add files via upload

---
+++ 01-JS-Syntax-Fundamentals/more-exercises/04-gladiator-expenses.js
@@ -0,0 +1,26 @@
+function calculateExpenses(lostFightsCount, helmetPrice, swordPrice, shieldPrice, armorPrice) {
+    let totalExpenses = 0;
+
+    for (let i = 1; i <= lostFightsCount; i++) {
+        if (i % 2 === 0) {
+            totalExpenses += helmetPrice;
+        }
+        if (i % 3 === 0) {
+            totalExpenses += swordPrice;
+        }
+        if (i % 6 === 0) {
+            totalExpenses += shieldPrice;
+        }
+        if (i % 12 === 0) {
+            totalExpenses += armorPrice;
+        }
+    }
+
+    console.log(`Gladiator expenses: ${totalExpenses.toFixed(2)} aureus`);
+}
+
+
+//test code
+
+calculateExpenses(7, 2, 3, 4, 5)
+calculateExpenses(23, 12.50, 21.50, 40, 200)
