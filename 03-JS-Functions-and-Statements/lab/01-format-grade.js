037d665148c68cc9bdfd9d830ea4344aad741f82
Lazarina-Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-03-10 16:44:00 +0100

    added task

---
+++ 03-JS-Functions-and-Statements/lab/01-format-grade.js
@@ -0,0 +1,24 @@
+function formatingGrade(grade) {
+    let result = '';
+
+    if (grade < 3.00) {
+        result += `Fail (2)`;
+    } else if (grade < 3.50) {
+        result += `Poor (${grade.toFixed(2)})`;
+    } else if (grade < 4.50) {
+        result += `Good (${grade.toFixed(2)})`;
+    } else if (grade < 5.50) {
+        result += `Very good (${grade.toFixed(2)})`;
+    } else if (grade <= 6.00) {
+        result += `Excellent (${grade.toFixed(2)})`;
+    }
+
+    console.log(result);
+}
+
+
+// test code
+
+formatingGrade(3.33);
+formatingGrade(4.50);
+formatingGrade(2.99);
