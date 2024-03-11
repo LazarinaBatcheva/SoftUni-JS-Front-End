4dc1de863e00e864bce40ab2de9f2c96b9aba919
Lazarina-Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-03-11 16:54:27 +0100

    added task

---
+++ 03-JS-Functions-and-Statements/exercises/01-smallest-of-three-numbers.js
@@ -0,0 +1,25 @@
+function findingSmallestNumber(num1, num2, num3) {
+    console.log(Math.min(num1, num2, num3));
+
+    // let smallestNumber = Infinity;
+    // let numbers = [num1, num2, num3];
+
+    // for (let number of numbers) {
+    //     if (number < smallestNumber) {
+    //         smallestNumber = number;
+    //     }
+    // }
+
+    // console.log(smallestNumber);
+}
+
+
+// test code
+
+findingSmallestNumber(1, 5, 3);
+findingSmallestNumber(600,
+    342,
+    123
+    );
+findingSmallestNumber(25, 21, 4);
+findingSmallestNumber(2, 2, 2);
