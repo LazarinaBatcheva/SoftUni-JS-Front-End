3832000a687867be50191576ef3395b4a2030679
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-03-15 17:34:20 +0100

    Add files via upload

---
+++ 03-JS-Functions-and-Statements/more-exercises/03-points-validation.js
@@ -0,0 +1,34 @@
+function checkValidDistance(pointsArray) {
+    const [x1, y1, x2, y2] = pointsArray.map(Number);
+
+    function calculateDistance(x1, y1, x2, y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
+    const distance1 = calculateDistance(x1, y1, 0, 0);
+    const distance2 = calculateDistance(x2, y2, 0, 0);
+    const distance3 = calculateDistance(x1, y1, x2, y2);
+
+    if (Number.isInteger(distance1)) {
+       console.log(`{${x1}, ${y1}} to {${0}, ${0}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${0}, ${0}} is invalid`);
+    }
+    if (Number.isInteger(distance2)) {
+        console.log(`{${x2}, ${y2}} to {${0}, ${0}} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {${0}, ${0}} is invalid`);
+    }
+    if (Number.isInteger(distance3)) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
+}
+
+
+// test code
+
+checkValidDistance([3, 0, 0, 4]);
+console.log('--------------------');
+checkValidDistance([2, 1, 1, 1]);
