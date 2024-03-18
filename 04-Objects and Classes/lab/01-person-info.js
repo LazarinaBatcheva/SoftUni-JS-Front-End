df4c79b073217e02ea462a88cc6d226cb67feb01
Lazarina-Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-03-18 15:34:46 +0100

    added task

---
+++ 04-Objects and Classes/lab/01-person-info.js
@@ -0,0 +1,22 @@
+function printPersonInfo(firstName, lastName, age) {
+    const personInfo = {
+        firstName,
+        lastName,
+        age,
+    }
+
+    return personInfo;
+}
+
+
+// test code
+
+printPersonInfo("Peter", 
+"Pan",
+"20"
+);
+console.log();
+printPersonInfo("George", 
+"Smith",
+"18"
+);
