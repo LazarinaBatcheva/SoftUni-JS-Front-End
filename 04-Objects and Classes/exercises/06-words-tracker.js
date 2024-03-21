8ec830848a94ce0b21d79d1da692ce8ab74b2fbc
Lazarina Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-03-21 13:22:26 +0100

    Add files via upload

---
+++ 04-Objects and Classes/exercises/06-words-tracker.js
@@ -0,0 +1,35 @@
+function printRepeatedWords(wordsList) {
+    const searchedWords = wordsList.shift().split(' ');
+    const repeatedWords = {};
+
+    for (const word of searchedWords) {
+        repeatedWords[word] = 0;
+    }
+
+    for (const word of wordsList) {
+        if (repeatedWords.hasOwnProperty(word)) {
+            repeatedWords[word] += 1;
+        }
+    }
+    
+    Object
+        .entries(repeatedWords)
+        .sort((a, b) => b[1] - a[1])
+        .forEach(([word, count]) => console.log(`${word} - ${count}`));
+}
+
+
+// test code
+
+printRepeatedWords([
+    'this sentence', 
+    'In', 'this', 'sentence', 'you', 'have', 'to', 'count', 'the', 'occurrences', 'of', 'the', 'words', 'this', 'and', 'sentence', 'because', 'this', 'is', 'your', 'task'
+    ]
+    );
+
+console.log();
+
+printRepeatedWords([
+    'is the', 
+    'first', 'sentence', 'Here', 'is', 'another', 'the', 'And', 'finally', 'the', 'the', 'sentence']
+    );
