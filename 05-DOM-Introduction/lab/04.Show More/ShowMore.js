5eea01b3b49550f4ee1be5a553dd344fd14ac10b
Lazarina-Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-03-30 18:52:36 +0100

    added task

---
+++ 05-DOM-Introduction/lab/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const readMoreElement = document.getElementById('more');
+    const hiddenTextElement = document.getElementById('text');
+
+    readMoreElement.style.display = 'none';
+    hiddenTextElement.style.display = 'inline';
+}
