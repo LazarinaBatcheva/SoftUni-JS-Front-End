1a784fd689593a89d7272407d2375c7ce215f064
Lazarina-Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-04-01 13:50:20 +0200

    added task

---
+++ 05-DOM-Introduction/exercises/04. Search in List/search.js
@@ -0,0 +1,17 @@
+function search() {
+   const townsListElement = document.getElementById('towns');
+   let searchedTextElement = document.getElementById('searchText').value;
+   const resultElement = document.getElementById('result');
+   let matchesCount = 0;
+
+   Array.from(townsListElement.children)
+      .forEach(town => {
+         if (town.textContent.toLowerCase().includes(searchedTextElement.toLowerCase())) {
+            town.style.fontWeight = 'bold';
+            town.style.textDecoration = 'underline';
+            matchesCount++;
+         }
+      });
+
+   resultElement.textContent = `${matchesCount} matches found`;
+}
