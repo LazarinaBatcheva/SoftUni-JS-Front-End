cc253d3f99d3082816f63b94b9a35f1ba8b63c6c
Lazarina-Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-04-06 19:24:10 +0200

    added task

---
+++ 06-DOM-Manipulation-and-Events/exercises/05. Encode-and-Decode-Messages/app.js
@@ -0,0 +1,31 @@
+function encodeAndDecodeMessages() {
+    const sendMsgElement = document.querySelector('#main > div:nth-child(1) textarea');
+    const receivedMsgElemen = document.querySelector('#main > div:nth-child(2) textarea');
+    const buttonElements = document.querySelectorAll('#main > div button');
+    const encodeButton = buttonElements[0];
+    const decodeButton = buttonElements[1];
+
+    encodeButton.addEventListener('click', () => {
+        const sentMessage = sendMsgElement.value;
+
+        let encodeMsg = '';
+        for (let i = 0; i < sentMessage.length; i++) {
+            let charCode = sentMessage.charCodeAt(i) + 1;
+            encodeMsg += String.fromCharCode(charCode);
+        }
+        receivedMsgElemen.textContent = encodeMsg;
+        sendMsgElement.value = '';
+    });
+
+    decodeButton.addEventListener('click', () => {
+        const receivedMessage = receivedMsgElemen.textContent;
+
+        let decodeMsg = '';
+        for (let i = 0; i < receivedMessage.length; i++) {
+            let charCode = receivedMessage.charCodeAt(i) - 1;
+            decodeMsg += String.fromCharCode(charCode);
+        }
+
+        receivedMsgElemen.textContent = decodeMsg;
+    });
+}
