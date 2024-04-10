e82e787c3f32a6b01dc723cfd8de180c54148119
Lazarina-Batcheva <134694151+LazarinaBatcheva@users.noreply.github.com>
2024-04-10 15:36:19 +0200

    added task

---
+++ 08-Asynchronous-Programming/02.Bus-Schedule/app.js
@@ -0,0 +1,40 @@
+function solve() {
+    const baseUrl = 'http://localhost:3030/jsonstore/bus/schedule/' ;
+
+    const infoElement = document.querySelector('#info span');
+    const departButtonElement = document.getElementById('depart');
+    const arriveButtonElement = document.getElementById('arrive');
+    let currentStopId = 'depot';
+    let nextStop = '';
+    
+
+    function depart() {
+        fetch(baseUrl + currentStopId)
+            .then(response => response.json())
+            .then( data => {
+                infoElement.textContent = `Next stop ${data.name}`;
+                currentStopId = data.next;
+                nextStop = data.name;
+                departButtonElement.disabled = true;
+                arriveButtonElement.disabled = false;
+            })
+        .catch ((error) => {
+            infoElement.textContent = 'Error';
+            departButtonElement.disabled = true;
+            arriveButtonElement.disabled = true;
+        });
+    }
+
+    async function arrive() {
+        infoElement.textContent = `Arriving at ${nextStop}`;
+        departButtonElement.disabled = false;
+        arriveButtonElement.disabled = true;
+    }
+
+    return {
+        depart,
+        arrive
+    };
+}
+
+let result = solve();
